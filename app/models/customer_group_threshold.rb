580786ae511336cdef5a88e8879a4d36d90aab27
github username <ingrey1@gmail.com>
2020-04-01 15:28:58 -0700

    added more migrations/models

---
+++ app/models/customer_group_threshold.rb
@@ -0,0 +1,2 @@
+class CustomerGroupThreshold < ApplicationRecord
+end
---
+++ db/migrate/20200401222417_create_customer_group_thresholds.rb
@@ -0,0 +1,10 @@
+class CreateCustomerGroupThresholds < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customer_group_thresholds do |t|
+      t.string :customerGroupName
+      t.decimal :rangeBottom
+      t.decimal :rangeTop
+      t.timestamps
+    end
+  end
+end
